6a98173034afdcceffdbc7fa81fcc64096677491
Alexandre142010 <aleqsandrovichmidelashvili@gmail.com>
2025-03-02 20:54:22 +0400

    commit

---
+++ level 06/Homework/lesson6.1.py
@@ -0,0 +1,6 @@
+Celsius= 10
+fahrenheit= Celsius * 9/5+32
+kelvin= Celsius+ 273.15
+print(Celsius)
+print(fahrenheit)
+print(kelvin)
---
+++ level 06/Homework/lesson6.py
@@ -0,0 +1,4 @@
+user= "Aleqsandre Midelashvili"
+age= 14
+city= "Tbilisi"
+print("Your name is"+" "+user+","+" "+"age is"+" "+"14"+","+" "+"living in"+" "+city)
