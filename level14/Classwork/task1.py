6dbba560dc3f415e9a4fc3fd42bb9cbc139542df
Alexandre142010 <aleqsandrovichmidelashvili@gmail.com>
2025-03-29 01:46:48 +0400

    commit

---
+++ level14/Classwork/task1.py
@@ -0,0 +1,8 @@
+#task1
+for number in range (10):
+    print(number)
+
+#task2
+name="Aleqsandre"
+for letter in name:
+    print(letter)
---
+++ level14/Homework/lesson14.1.py
@@ -0,0 +1,31 @@
+#task1
+for numer in range (20):
+    print(numer)
+#task2
+
+for number in range (31):
+    print(number)
+
+#task3
+for number in range (15,80) :
+    print(number)
+
+#task4
+for number in range (0,65) :
+    print(number)
+
+#task5
+for number in range (0,51,3) :
+    print(number)
+
+#task6
+for number in range (51) :
+    print(number)
+
+#task7
+for number in range(0,100,2) :
+    print(number)
+
+#task8
+for i in range (20,-1,-1) :
+    print(i)
