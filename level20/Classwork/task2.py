464b187c4a61bd9166ec4b4308bcc202034d0c80
Alexandre142010 <aleqsandrovichmidelashvili@gmail.com>
2025-04-17 14:04:26 +0400

    commit

---
+++ level20/Classwork/task2.py
@@ -0,0 +1,9 @@
+number = int(input("Enter number: "))
+if number > 0 :
+    print("Number is positive")
+if number %2 == 0:
+    print("Even")
+elif number %2 == 1:
+    print("Odd")
+if number <0 : 
+    print("Number is negative")
---
+++ level20/Classwork/task3.py
@@ -0,0 +1,8 @@
+vegitables = ["Carrot", "Tomato", "Broccoli", "Onion"]
+for veg in vegitables:
+    print(veg)
+
+print(vegitables[0])
+print(vegitables[1])
+print(vegitables[2])
+print(vegitables[3])
